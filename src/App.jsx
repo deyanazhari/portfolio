d7ba3ffa2c80ed81ec876077df7274bacfa998ed
deyanazhari1 <deyanazhari@gmail.com>
2025-07-04 23:44:33 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-    {/* <h1 className="text-4xl text-red-600">Hello World</h1> */}
+    <h1 className="text-3xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, doloribus voluptas corrupti accusamus natus quaerat quam sequi dolorum deserunt at totam, veritatis ipsum quae sunt enim aliquam dolor qui ea cumque. Dolore amet modi optio veniam maiores sed doloribus aperiam consequatur vero similique harum voluptate, velit fugiat odit numquam aut natus hic aliquam! Tenetur voluptas ratione minus libero quos illo doloribus eius nesciunt id harum. Beatae consequuntur consectetur quia alias ipsam eveniet minima repellendus fuga a veniam quos officiis ex at dolore suscipit maiores facere delectus, non blanditiis totam sed cumque! Quisquam exercitationem repellat, unde est architecto odio fuga? Dolores voluptas iure magnam voluptates? Obcaecati sequi iure sint quibusdam officiis voluptate numquam voluptates velit reprehenderit ad, dolor sunt tempora voluptas nemo dolore dolorem esse perferendis unde eos alias consequatur illo fugit delectus beatae! Quos vel autem, minus libero voluptatibus a, dolor veniam reprehenderit amet porro voluptate asperiores hic fugiat, rem odit molestiae veritatis soluta molestias ratione unde obcaecati labore! Voluptas beatae eveniet necessitatibus at quae veniam magnam debitis molestiae accusantium. Laudantium fugiat dolorem dolore repudiandae. Perspiciatis officiis repudiandae dolorum vitae iure illum ipsa cumque voluptate dolorem saepe odio possimus repellendus nulla distinctio, numquam autem veritatis cupiditate, qui nobis quidem voluptatum.</h1>
 
     </>
   )
