022b28a3afd3a31d87cbbe5f0a1cdbc928ab4155
deyanazhari1 <deyanazhari@gmail.com>
2025-07-04 23:05:39 +0700

    Add futuristic DA icon in SVG and PNG formats

---
+++ src/components/navbar.jsx
@@ -0,0 +1,26 @@
+
+const navbar = () => {
+  return (
+    <div className="navbar py-7 flex items-center justify-between">
+        <div className="logo">
+            <h1>Deyan Azhari</h1>
+            <ul className="menu">
+            <li>
+                <a href="#">Beranda</a>
+            </li>
+            <li>
+                <a href="#">Tentang</a>
+            </li>
+             <li>
+                <a href="#">Work</a>
+            </li>
+            <li>
+                <a href="#">Kontak</a>
+            </li>
+            </ul> 
+        </div>
+    </div>
+  )
+}
+
+export default navbar;
